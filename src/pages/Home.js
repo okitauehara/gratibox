263dd5c12f3426d26b0a8001ac0abeb19efbc151
okitauehara <marcosokita@live.com>
2021-11-17 18:47:37 -0300

    Changing styles to a new component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,74 +1,23 @@
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import * as S from '../styles/HomeStyles';
 import home from '../assets/home.jpg';
 
 function Home() {
   const navigate = useNavigate();
 
   return (
-    <PageStyle>
-      <Title>Bem vindo ao GratiBox</Title>
-      <Subtitle>
+    <S.PageStyle>
+      <S.Title>Bem vindo ao GratiBox</S.Title>
+      <S.Subtitle>
         Receba em casa um box com chás, produtos organicos, incensos e muito mais...
-      </Subtitle>
-      <HomeImg src={home} alt="Yoga Girl" />
-      <BackgroundColor>
-        <Button onClick={() => navigate('/sign-up')}>Quero começar</Button>
-        <Redirect onClick={() => navigate('/sign-in')}>Já sou grato</Redirect>
-      </BackgroundColor>
-    </PageStyle>
+      </S.Subtitle>
+      <S.HomeImg src={home} alt="Yoga Girl" />
+      <S.BackgroundColor>
+        <S.Button onClick={() => navigate('/sign-up')}>Quero começar</S.Button>
+        <S.Redirect onClick={() => navigate('/sign-up')}>Já sou grato</S.Redirect>
+      </S.BackgroundColor>
+    </S.PageStyle>
   );
 }
 
 export default Home;
-
-const PageStyle = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 30px 30px 0px 30px;
-`;
-
-const Title = styled.h1`
-  font-size: 28px;
-  margin-bottom: 45px;
-`;
-
-const Subtitle = styled.h2`
-  font-size: 18px;
-  font-weight: 300;
-  text-align: center;
-`;
-
-const HomeImg = styled.img`
-  width: 100vw;
-  height: fit-content;
-`;
-
-const BackgroundColor = styled.div`
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: #4D65A8;
-`;
-
-const Button = styled.button`
-  width: calc(100% - 60px);
-  height: 45px;
-  background-color: #8C97EA;
-  font-family: 'Roboto', sans-serif;
-  font-size: 18px;
-  font-weight: 700;
-  color: #FFFFFF;
-  border: none;
-  border-radius: 10px;
-  margin: 15px 85px;
-`;
-
-const Redirect = styled.p`
-  font-size: 18px;
-  line-height: 1.5;
-  margin-bottom: 32px;
-`;
---
+++ src/styles/HomeStyles.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+const PageStyle = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 30px 30px 0px 30px;
+`;
+
+const Title = styled.h1`
+  font-size: 28px;
+  margin-bottom: 45px;
+`;
+
+const Subtitle = styled.h2`
+  font-size: 18px;
+  font-weight: 300;
+  text-align: center;
+`;
+
+const HomeImg = styled.img`
+  width: 100vw;
+  height: fit-content;
+`;
+
+const BackgroundColor = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #4D65A8;
+`;
+
+const Button = styled.button`
+  width: calc(100% - 60px);
+  height: 45px;
+  background-color: #8C97EA;
+  font-family: 'Roboto', sans-serif;
+  font-size: 18px;
+  font-weight: 700;
+  color: #FFFFFF;
+  border: none;
+  border-radius: 10px;
+  margin: 15px 85px;
+`;
+
+const Redirect = styled.p`
+  font-size: 18px;
+  line-height: 1.5;
+  margin-bottom: 32px;
+`;
+
+export {
+  PageStyle,
+  Title,
+  Subtitle,
+  HomeImg,
+  BackgroundColor,
+  Button,
+  Redirect,
+};
