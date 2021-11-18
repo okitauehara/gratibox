d21f2c1cb2f7d402407d20f8c9af0a7cb782827c
okitauehara <marcosokita@live.com>
2021-11-17 23:07:56 -0300

    Base layout and validations for sign-in page implemented

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,6 +1,49 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Forms, Input, Button } from '../styles/AccessStyle';
+import { PageStyle, Title, Redirect } from '../styles/HomeStyles';
+
 function SignIn() {
+  const [values, setValues] = useState({ email: '', password: '' });
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  const submitSignIn = (event) => {
+    event.preventDefault();
+    setIsDisabled(true);
+  };
+
   return (
-    <h1>SignIn</h1>
+    <PageStyle>
+      <Title>Bem vindo ao GratiBox</Title>
+      <Forms onSubmit={submitSignIn}>
+        <Input
+          required
+          placeholder="E-mail"
+          type="email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+          disabled={isDisabled}
+        />
+        <Input
+          required
+          placeholder="Senha"
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+          disabled={isDisabled}
+        />
+        <Button type="submit" disabled={isDisabled}>Login</Button>
+      </Forms>
+      <Link to="/sign-up" style={{ pointerEvents: isDisabled ? 'none' : 'all', color: '#FFFFFF' }}>
+        <Redirect>Ainda não sou grato</Redirect>
+      </Link>
+    </PageStyle>
   );
 }
 
