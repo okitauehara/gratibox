3014f6f021e17bd2b531a0f10aa6e6e1089a81e5
okitauehara <marcosokita@live.com>
2021-11-17 23:48:08 -0300

    Adding Provider to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
 import { BrowserRouter as Router } from 'react-router-dom';
+import { useState } from 'react';
 import GlobalStyle from './styles/GlobalStyle';
 import Pages from './Pages';
+import UserContext from './contexts/UserContext';
 
 function App() {
+  const userData = JSON.parse(localStorage.getItem('@user'));
+  const [user, setUser] = useState(userData);
+
   return (
-    <Router>
-      <GlobalStyle />
-      <Pages />
-    </Router>
+    <UserContext.Provider value={{ user, setUser }}>
+      <Router>
+        <GlobalStyle />
+        <Pages />
+      </Router>
+    </UserContext.Provider>
   );
 }
 
