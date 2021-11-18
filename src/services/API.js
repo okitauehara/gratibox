c910e2f91c1962f79736d6719e7f6df7634e9e8f
okitauehara <marcosokita@live.com>
2021-11-17 23:34:09 -0300

    Sign-in API function implemented

--- src/services/API.js
+++ src/services/API.js
@@ -1,8 +1,19 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://gratibox-api.herokuapp.com';
+const BASE_URL = 'http://localhost:4000';
+// https://gratibox-api.herokuapp.com
 
-export default function postSignUp(body) {
+function postSignUp(body) {
   const promise = axios.post(`${BASE_URL}/sign-up`, body);
   return promise;
 }
+
+function postSignIn(body) {
+  const promise = axios.post(`${BASE_URL}/sign-in`, body);
+  return promise;
+}
+
+export {
+  postSignUp,
+  postSignIn,
+};
