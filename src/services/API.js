3b2ca8dabcf38aa1d22cc79502e3ca87f1a20102
okitauehara <marcosokita@live.com>
2021-11-17 22:46:07 -0300

    Changing BASE_URL to heroku server address

--- src/services/API.js
+++ src/services/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = 'https://gratibox-api.herokuapp.com';
 
 export default function postSignUp(body) {
   const promise = axios.post(`${BASE_URL}/sign-up`, body);
