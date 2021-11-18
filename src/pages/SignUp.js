9f3f86e7d6ec156cbbfe10512588b0d53ccd81ab
okitauehara <marcosokita@live.com>
2021-11-17 22:36:34 -0300

    Connection front-back signup implemented

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,20 +1,52 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import Swal from 'sweetalert2';
 import { PageStyle, Redirect, Title } from '../styles/HomeStyles';
+import postSignUp from '../services/API';
 
 function SignUp() {
   const [values, setValues] = useState({ name: '', email: '', password: '' });
   const [isDisabled, setIsDisabled] = useState(false);
   const emailRegex = '[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}';
+  const navigate = useNavigate();
 
   const handleChange = (event) => {
     setValues({ ...values, [event.target.name]: event.target.value });
   };
 
+  const success = async () => {
+    await Swal.fire({
+      icon: 'success',
+      title: 'Usuário cadastrado!',
+    });
+    setIsDisabled(false);
+    navigate('/sign-in');
+  };
+
+  const error = (err) => {
+    if (err.response.status === 400) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Verifique se todos os dados inseridos são válidos',
+      });
+    }
+    if (err.response.status === 409) {
+      Swal.fire({
+        icon: 'error',
+        title: 'O e-mail inserido já está em uso',
+      });
+    }
+    setIsDisabled(false);
+  };
+
   const submitSignUp = (event) => {
     event.preventDefault();
     setIsDisabled(true);
+    const body = values;
+    postSignUp(body)
+      .then(success)
+      .catch(error);
   };
 
   return (
