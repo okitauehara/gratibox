074f7441c043422caaeec1d60028befc186df98c
okitauehara <marcosokita@live.com>
2021-11-18 13:10:14 -0300

    Private Route and logics implemented

--- src/pages/Plans.js
+++ src/pages/Plans.js
@@ -1,21 +1,54 @@
 import styled from 'styled-components';
+import { useContext, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import { PageStyle, Subtitle, Title } from '../styles/HomeStyles';
 import weekImg from '../assets/week_plan.jpg';
 import monthImg from '../assets/month_plan.jpg';
+import UserContext from '../contexts/UserContext';
 
 function Plans() {
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(async () => {
+    if (!user) {
+      await Swal.fire({
+        title: 'Login necessário',
+        text: 'Para acessar essa rota, você precisa estar logado',
+        icon: 'warning',
+        showDenyButton: true,
+        confirmButtonText: 'Fazer Login',
+        denyButtonText: 'Ir para Home',
+        confirmButtonColor: '#8C97EA',
+        denyButtonColor: '#AAA',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate('/sign-in');
+        } else {
+          navigate('/');
+        }
+      });
+    }
+  }, []);
+
   return (
-    <PageStyle>
-      <Title>Bom te ver por aqui, @User.</Title>
+    <PageStyle style={{ marginBottom: '30px' }}>
+      <Title>
+        Bom te ver por aqui,
+        {' '}
+        {user?.name}
+      </Title>
       <Subtitle>Você ainda não assinou um plano, que tal começar agora?</Subtitle>
       <Container>
         <ImgBox src={weekImg} alt="Yoga Girl With Plants" />
         <PlanDescription>
           Você recebe um box por semana.
           Ideal para quem quer exercer a gratidão todos os dias.
-
         </PlanDescription>
-        <Button>Assinar</Button>
+        <Link to="/subscription/1">
+          <Button>Assinar</Button>
+        </Link>
       </Container>
       <Container>
         <ImgBox src={monthImg} alt="Yoga Girl In Her Bedroom" />
@@ -23,7 +56,9 @@ function Plans() {
           Você recebe um box por mês.
           Ideal para quem está começando agora.
         </PlanDescription>
-        <Button>Assinar</Button>
+        <Link to="/subscription/2">
+          <Button>Assinar</Button>
+        </Link>
       </Container>
     </PageStyle>
   );
